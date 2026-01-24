79e568a2380ca5f95557ebc405ed489ec84f2a71
Gabriel De León <xgabrieldeleon@gmail.com>
2026-01-24 16:37:56 -0300

    create spring boot structure for backend

---
+++ backend/src/main/java/uy/eleven/canasta/CanastaApplication.java
@@ -0,0 +1,13 @@
+package uy.eleven.canasta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanastaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanastaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/uy/eleven/canasta/CanastaApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.eleven.canasta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanastaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
